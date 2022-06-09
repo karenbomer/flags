4ef369403ddb60ba237659f9a3b5dfe97439b962
karenbomer <karenmaciel@outlook.fr>
2022-06-09 18:53:04 +0200

    annuler la recherche

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -5,6 +5,9 @@ import Card from './Card';
 const Countries = () => {
     const [data, setData] = useState([])
     const [rangeValue, setRangeValue] = useState(36);
+    const [selectedRadio, setSelectedRadio] = useState("");
+    const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
     useEffect(() => {
         axios.get("https://restcountries.com/v3.1/all")
         .then((result) =>setData(result.data));
@@ -16,13 +19,32 @@ const Countries = () => {
                 <input 
                 type="range" min="1" max="250" 
                 defaultValue={rangeValue}
-                 onChange={(event) => setRangeValue(event.target.value)} />
+                onChange={(event) => setRangeValue(event.target.value)} />
+                {radios.map((continent) => (
+                    <li>
+                        <input 
+                            type="radio" 
+                            id={continent} 
+                            name="continentRadio" 
+                            checked={continent === selectedRadio}
+                            onChange={(event) => setSelectedRadio(event.target.id)} 
+                            />
+                        <label htmlFor={continent}>{continent}</label>
+                    </li>
+                ))}
             </ul>
+            {selectedRadio && (
+            <button onClick={() => setSelectedRadio("")}>
+                Annuler la recherche
+            </button>
+            )}
             <ul>
                 {data
+                    .filter((country) => country.continents[0].includes(selectedRadio))
+                    .sort((a,b) => b.population - a.population)
                     .slice(0, rangeValue)
                     .map((country, index) => (
-                   <Card key={index} country={country} />
+                    <Card key={index} country={country} />
                 ))}
             </ul>
         </div>
