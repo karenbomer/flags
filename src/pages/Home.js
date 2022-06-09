e86abb99d641fddbf6183149ab9e28ff07ea9d26
karenbomer <karenmaciel@outlook.fr>
2022-06-09 17:22:38 +0200

    flags project

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from '../components/Nav';
+
+const Home = () => {
+    return (
+        <div>
+            <Nav />
+            <h1>Accueil</h1>
+        </div>
+    );
+};
+
+export default Home;
