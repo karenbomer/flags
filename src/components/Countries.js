508631183d732755d281567dab2ecf67a568c2a9
karenbomer <karenmaciel@outlook.fr>
2022-06-09 18:09:08 +0200

    affichage drapeaux ok

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Card from './Card';
 
 const Countries = () => {
     const [data, setData] = useState([])
@@ -11,6 +12,11 @@ const Countries = () => {
     return (
         <div className="countries">
             <h1>All countries</h1>
+            <ul>
+                {data.map((country, index) => (
+                   <Card key={index} country={country} />
+                ))}
+            </ul>
         </div>
     );
 };
